ba460e2141dcd6183eb0300bd60d60c78f439e05
Bashir Ahmed <bashirahmedmangno110@gmail.com>
2024-03-03 21:34:13 +0500

    Assignment No_1

---
+++ JP_Dart_Flutter-Master/Assignment_1/lengthBredth.dart
@@ -0,0 +1,12 @@
+void main() {
+//Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area of the rectangle.
+//Formula (area = length * breadth)
+  int length = 5;
+  int bredth = 7;
+  int area =  length * bredth;
+  print("The area of a Rectangle is $area");
+}
+
+
+
+
