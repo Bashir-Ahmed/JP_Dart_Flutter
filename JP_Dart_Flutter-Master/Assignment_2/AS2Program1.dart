f2430e0315f1efd29afd6c4a23e7262c1c042a83
Bashir Ahmed <bashirahmedmangno110@gmail.com>
2024-03-03 23:18:31 +0500

    Usage of Conditional Statement to check Results

---
+++ JP_Dart_Flutter-Master/Assignment_2/AS2Program1.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  //Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+//i.e: if both values are equal then it's square otherwise rectangle.
+
+int length = 70;
+int breadth = 40;
+
+if(length == breadth)
+{
+//i.e: if both values are equal then it's square
+  print("Squre");
+
+}
+else 
+{
+  // otherwise rectangle.
+  print("Rectangle");
+}
+}
