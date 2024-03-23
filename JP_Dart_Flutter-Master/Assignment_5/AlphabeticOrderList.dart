d8be40f90e470b009d185790358fb2cf2214fbbb
Bashir Ahmed <bashirahmedmangno110@gmail.com>
2024-03-23 05:18:42 +0500

    Alphebet Order List

---
+++ JP_Dart_Flutter-Master/Assignment_5/AlphabeticOrderList.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  //Q.1: Write a Dart program to create a list of your favorite fruits and print them in alphabetical order.
+  List<String> fruits = ['Pineapple', 'Mango','Banana','Starwberry','Orange'];
+  fruits.sort();
+  print(fruits);
+}
