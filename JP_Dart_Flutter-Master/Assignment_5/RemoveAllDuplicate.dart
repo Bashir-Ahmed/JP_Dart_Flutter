8188f51014986d77524f952960903e8b08227ccc
Bashir Ahmed <bashirahmedmangno110@gmail.com>
2024-03-23 05:37:57 +0500

    Remove Duplicate Values from List

---
+++ JP_Dart_Flutter-Master/Assignment_5/RemoveAllDuplicate.dart
@@ -0,0 +1,22 @@
+void main(){
+//Q.3: Write a Dart program to remove all duplicate elements from a given list of numbers.
+List<String> fruits = [
+    "Banana", 
+    "Pineapple", 
+    "Strawberry",
+    "Banana",
+    "Mangno",
+    "Pineapple",
+    "Orange",
+];
+
+var seen = Set<String>();
+List<String> uniquelist = fruits.where((fruits1) => seen.add(fruits1)).toList();
+print(uniquelist);
+
+
+
+
+
+
+}
