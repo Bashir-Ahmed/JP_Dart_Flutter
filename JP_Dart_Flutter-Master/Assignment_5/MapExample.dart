da4e413ba047982316d4687b359ff1ef6de5fdf3
Bashir Ahmed <bashirahmedmangno110@gmail.com>
2024-03-23 05:59:36 +0500

    Map Example

---
+++ JP_Dart_Flutter-Master/Assignment_5/MapExample.dart
@@ -0,0 +1,6 @@
+void main()
+{
+  //Q.6: Write a Dart program to create a map representing a student's information including name, age, and grade. Print out all the information stored in the map.
+  var bioData = {'Name':'Bashir','Age':'28','Grade':"A"};
+  print(bioData);
+}
