a28325d14d09fbe925ce4c9bc35c29b6fd29db6a
Bashir Ahmed <bashirahmedmangno110@gmail.com>
2024-03-30 08:39:05 +0500

    Assignment of Map

---
+++ JP_Dart_Flutter-Master/Assignment_6/MapExamples.dart
@@ -0,0 +1,24 @@
+void main()
+{
+  //Q.6: Create Map variable name world then inside it create countries Map, Key will be the name country & country value will have another map having capitalCity, currency and language to it. by using any country key print all the value of Capital & Currency.
+
+
+  Map world = {"Countries": {
+                 "Pakistan": {
+                  "Capital":"Islamabad",
+                  "Language":"Urdu",
+                  "Currency":"PKR"
+
+                 },
+                 "India":{
+                  "Capital":"New Dehli",
+                  "Language":"Hindhi",
+                  "Currency":"Indian Rupees"
+                 }
+                 }
+                 
+  
+               };
+               
+               print(world['Countries']['Pakistan']['Capital']);
+}
---
+++ JP_Dart_Flutter-Master/Assignment_6/MapExmapleOfLength.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  //Q.5: Create a map with name, phone keys and store some values to it. Use where to find all keys that have length 4.
+  Map phoneKey = {'Iphone':'50000','Oppo':'40000','Infinix':'30000'};
+ 
+  print(phoneKey.keys);
+
+}
